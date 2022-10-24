35ba2a2009fb45b0f427d05a1f1c405ad6af5058
Kent Hu <khu.1998@icloud.com>
2022-10-24 22:38:26 +1100

    Add failing test

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -3,9 +3,11 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class CalculatorTest {
     private static int NUMBER = 0;
+
     @Test
     void flake() {
         ++NUMBER;
@@ -16,5 +18,10 @@ class CalculatorTest {
     void add() {
         assertEquals(1 + 2, Calculator.add(1, 2));
         assertEquals(4 + 5, Calculator.add(4, 5));
+    }
+
+    @Test
+    void fails() {
+        fail();
     }
 }
